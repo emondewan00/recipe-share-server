c8892c8c42a8d4fbcb976ed2fd661e41a160b509
emondewan00 <dewanmohammademon@gmail.com>
2024-05-26 06:37:36 +0600

    update get all recipe route

--- handler/recipeHandler.js
+++ handler/recipeHandler.js
@@ -5,12 +5,60 @@ import { getDb } from "../utils/connectDb.js";
 
 const router = express.Router();
 
-// Get all recipes
+//get all recipes
 router.get("/", async (req, res) => {
   try {
     const db = getDb(); // Get MongoDB database instance
-    const recipes = await db.collection("recipes").find({}).toArray();
-    res.status(200).json(recipes);
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page if not provided
+    const skip = (page - 1) * limit;
+
+    let query = {};
+    if (req.query.name) {
+      query.name = { $regex: req.query.name, $options: "i" };
+    }
+    if (req.query.category) {
+      query.category = { $regex: req.query.category, $options: "i" };
+    }
+    if (req.query.country) {
+      query.country = { $regex: req.query.country, $options: "i" };
+    }
+    const totalLength = await db.collection("recipes").find(query).count();
+
+    // calculate how many pages are available
+    const totalPages = Math.ceil(totalLength / limit);
+    const morePages = totalPages > page;
+
+    // If the page number is greater than the total number of pages, return an error
+    if (page > totalPages) {
+      return res.status(400).json({
+        status: "error",
+        message: `Page ${page} does not exist. There are only ${totalPages} pages.`,
+      });
+    }
+
+    const recipes = await db
+      .collection("recipes")
+      .find(query)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+
+    if (recipes.length === 0 && skip !== 0) {
+      return res.status(200).json({
+        status: "success",
+        recipes: [],
+        length: recipes.length,
+        morePages,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      recipes,
+      length: recipes.length,
+      morePages,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -45,6 +93,36 @@ router.post("/", verifyJwt, async (req, res) => {
       purchased_by: [],
       createdAt: Date.now(),
     });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.patch("/purchased", verifyJwt, async (req, res) => {
+  try {
+    const { recipe_id, email, creatorEmail } = req.body;
+    const db = getDb(); // Get MongoDB database instance
+    const result = await db
+      .collection("recipes")
+      .findOneAndUpdate(
+        { _id: new ObjectId(recipe_id) },
+        { $push: { purchased_by: email } },
+        { returnOriginal: false }
+      );
+
+    const addCoinInCreatorAccount = await db
+      .collection("users")
+      .findOneAndUpdate(
+        {
+          creatorEmail: creatorEmail,
+        },
+        { $inc: { coins: 1 } }
+      );
+    const removeCoinsFromBuyer = await db
+      .collection("users")
+      .findOneAndUpdate({ email: email }, { $inc: { coins: -10 } });
+    console.log(result,removeCoinsFromBuyer,addCoinInCreatorAccount);
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
